39602577782e914d096bf513c1cd1cfdf285646c
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2024-01-02 23:16:25 +0530

    Delete Ex1 directory

--- Ex1/Fibonacci.py
+++
@@ -1,7 +0,0 @@
-def fibonacci(n):
-    if (n==1 or n==2):
-        return 1
-    else:
-        return fibonacci(n-1)+fibonacci(n-2)
-for i in range(1,11):
-    print(fibonacci(i))
--- Ex1/TowerofHanoi.py
+++
@@ -1,9 +0,0 @@
-def TowerOfHanoi(n,src,aux,dest):
-    if (n==1):
-        print(src+"=>"+dest)
-        return
-    else:
-        TowerOfHanoi(n-1,src,dest,aux)
-        TowerOfHanoi(1,src,aux,dest)
-        TowerOfHanoi(n-1,aux,src,dest)
-TowerOfHanoi(5,'A','B','C')
